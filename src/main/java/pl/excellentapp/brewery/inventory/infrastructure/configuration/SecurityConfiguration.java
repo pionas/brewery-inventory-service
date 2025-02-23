517118cda752c0af4c498bf9ceff678dbbdcd15d
Adrian Pionka <adrian@pionka.com>
2025-02-23 18:38:45 +0100

    add security configuration

---
+++ src/main/java/pl/excellentapp/brewery/inventory/infrastructure/configuration/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package pl.excellentapp.brewery.inventory.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration(proxyBeanMethods = false)
+@EnableWebSecurity
+class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .httpBasic(withDefaults())
+                .build();
+    }
+}
