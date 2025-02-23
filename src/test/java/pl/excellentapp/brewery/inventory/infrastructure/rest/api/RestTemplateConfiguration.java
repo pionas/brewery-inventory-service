d3e83991ac73abec65316b45249385783a4868d7
Adrian Pionka <adrian@pionka.com>
2025-02-23 13:11:45 +0100

    upgrade parent to 1.0.5 version

--- src/test/java/pl/excellentapp/brewery/inventory/infrastructure/rest/api/RestTemplateConfiguration.java
+++ src/test/java/pl/excellentapp/brewery/inventory/infrastructure/rest/api/RestTemplateConfiguration.java
@@ -1,8 +1,8 @@
 package pl.excellentapp.brewery.inventory.infrastructure.rest.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.DefaultResponseErrorHandler;
@@ -10,7 +10,7 @@ import org.springframework.web.client.DefaultResponseErrorHandler;
 @TestConfiguration
 class RestTemplateConfiguration {
 
-    @LocalServerPort
+    @Value("${server.port}")
     int localServerPort;
 
     @Bean
